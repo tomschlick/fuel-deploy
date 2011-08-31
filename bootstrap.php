ae377514c37d9579efc696122731cf1452c1b657
Tom Schlick <tom@tomschlick.com>
2011-08-31 13:05:34 -0400

    adding bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Deploy package for Fuel
+ *
+ * @package		Deploy
+ * @version		1.0
+ * @author		Tom Schlick (tom@tomschlick.com)
+ * @link		http://github.com/tomschlick/fuel-deploy
+ * 
+ */
