77bbfccb0faface35305b636ba7007ca30ea87b5
Tom Schlick <tom@tomschlick.com>
2011-08-31 13:11:51 -0400

    adding the commands

--- tasks/deploy.php
+++ tasks/deploy.php
@@ -19,21 +19,23 @@ class Deploy {
 	public static function git()
 	{
 		\Cli::write('Updating Git...');
-		
+		exec(\Config::get('deploy.'.\Config::get('environment').'.git_command'), $out);
+		\Cli::write($out);
 		\Cli::write('Git Updated.', 'green');
 	}
 	
 	public static function cache()
 	{
 		\Cli::write('Clearing the cache...');
-		
+		\Cache::delete_all();
 		\Cli::write('Cache Cleared.', 'green');
 	}
 	
 	public static function migrate()
 	{
 		\Cli::write('Starting Migration Process...');
-		
+		exec(\Config::get('deploy.'.\Config::get('environment').'.migrate_command'), $out);
+		\Cli::write($out);
 		\Cli::write('Migration Completed.', 'green');
 	}
 }
