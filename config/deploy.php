bb7e8fe59ed4257226079115f0cf4481cff127ac
Tom Schlick <tom@tomschlick.com>
2011-08-31 13:34:04 -0400

    adding submodule init/update

--- config/deploy.php
+++ config/deploy.php
@@ -3,12 +3,12 @@
 return array(
 	
 	'test'				=> array(
-		'git_command'		=> 'git checkout master; git pull origin master;',
+		'git_command'		=> 'git checkout master; git pull origin master; git submodule init; git submodule update;',
 		'migrate_command'	=> 'php oil r migrate',
 		),
 		
 	'production'		=> array(
-		'git_command'		=> 'git checkout master; git pull origin master;',
+		'git_command'		=> 'git checkout master; git pull origin master; git submodule init; git submodule update;',
 		'migrate_command'	=> 'php oil r migrate',
 		),
 	
